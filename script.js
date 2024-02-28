f63f5c0a6c35b65b6369e19a3e9e74fc6a13a4dc
Kevin <rosemkvnp@gmail.com>
2024-02-27 20:38:07 -0800

    finished homepage

--- script.js
+++ script.js
@@ -19,4 +19,19 @@ function slideChanger() {
 
     }, 300); 
 }
-setInterval(slideChanger, 10000);
+setInterval(slideChanger, 10000);   
+
+let b 
+let roofingContent = document.querySelector('.roofingContent')
+
+let roofingContentTxt =document.querySelector('.roofingContentTxt')
+
+roofingContent.forEach(function(content){
+    roofingContent.addEventListener('mouseover', function(){
+       roofingContentTxt.style.color -'red'
+    })
+})
+
+
+
+     
